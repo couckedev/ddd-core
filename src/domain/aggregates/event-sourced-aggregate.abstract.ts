ab30b296407862bbfd8f53b6fac57d1c98ca18b1
couckedev <coucke.dev@gmail.com>
2026-03-20 12:07:16 +0100

    refactor!: move streamId/revision stamping to infrastructure
    BREAKING CHANGE: Domain events no longer contain streamId/revision and
    aggregates rehydrate from StreamEvent history items.
    WHY: Because stream id and revision stamping are supposed to be on
    infrastructure side

--- src/domain/aggregates/event-sourced-aggregate.abstract.ts
+++ src/domain/aggregates/event-sourced-aggregate.abstract.ts
@@ -1,13 +1,15 @@
 import { AbstractDomainEvent } from "../events/domain-event.abstract.js";
+import { StreamEvent } from "../events/stream-event.type.js";
 import { AbstractBasicAggregate } from "./basic-aggregate.abstract.js";
 
 export abstract class AbstractEventSourcedAggregate<
   EventType extends AbstractDomainEvent,
 > extends AbstractBasicAggregate {
-  get streamId(): string {
-    return `${this.aggregateType}-${this.aggregateId}`;
-  }
-
+  /**
+   * Represents the last persisted revision of this aggregate (rehydrated from history).
+   * It MUST NOT be incremented when recording new domain events.
+   * The infrastructure layer is responsible for assigning revisions and enforcing optimistic locking.
+   */
   protected _currentRevision: number = -1;
 
   private _pending_events: EventType[] = [];
@@ -19,9 +21,7 @@ export abstract class AbstractEventSourcedAggregate<
   protected abstract applyEvent<Event extends EventType>(event: Event): void;
 
   protected recordEvent(event: EventType): void {
-    const revision = this._currentRevision + 1;
-    event._stamp(this.streamId, revision);
-    this._currentRevision = revision;
+    this.applyEvent(event);
     this._pending_events.push(event);
   }
 
@@ -33,10 +33,10 @@ export abstract class AbstractEventSourcedAggregate<
     this._pending_events = [];
   }
 
-  public rehydrate(events: EventType[]): this {
-    for (const event of events) {
-      this.applyEvent(event);
-      this._currentRevision = event.revision;
+  public rehydrate(history: StreamEvent<EventType>[]): this {
+    for (const item of history) {
+      this.applyEvent(item.event);
+      this._currentRevision = item.revision;
     }
     return this;
   }
--- src/domain/index.ts
+++ src/domain/index.ts
@@ -3,3 +3,4 @@ export * from "./aggregates/event-sourced-aggregate.abstract.js";
 export * from "./errors/aggregate-event-application.error.js";
 export * from "./errors/business.error.js";
 export * from "./events/domain-event.abstract.js";
+export type * from "./events/stream-event.type.js";
